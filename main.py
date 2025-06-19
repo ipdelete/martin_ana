701dd9a4d6ca275da6d3581d39049c4c7db81dad
Ian Philpot <ian.philpot@microsoft.com>
2025-06-19 10:43:16 -0400

    Project Init, prop.sh setup of data

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fun-analyitics!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ prep.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+sqlite-utils insert data/wildlife.db occurrence data/occurrence.csv --csv --delimiter=$'\t'
+sqlite-utils data/saintmartin_wildlife.db -t "select species, decimalLatitude, decimalLongitude from occurrence limit 2"
+
+sqlite-utils transform data/wildlife.db occurrence \
+  --rename decimalLatitude latitude \
+  --rename decimalLongitude longitude \
+  --drop eventDate \
+  --drop institutionCode \
+  --drop collectionCode \
+  --drop catalogNumber \
+  --drop recordNumber \
+  --drop basisOfRecord \
+  --drop identifiedBy \
+  --drop recordedBy \
+  --drop occurrenceStatus \
+  --pk id
+
+datasette install datasette-cluster-map
+datasette data/wildlife.db
